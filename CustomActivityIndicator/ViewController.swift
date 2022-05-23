42d87618245d9a8fddb7bc71f71f700e75ad5ddb
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-05-23 16:09:02 +0300

    Initial Commit

---
+++ CustomActivityIndicator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomActivityIndicator
+//
+//  Created by Олейник Богдан on 23.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
